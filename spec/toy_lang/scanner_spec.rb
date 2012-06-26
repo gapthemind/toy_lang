f25cbc3bbc452f675afd97b845905bbedf7decef
Diego Alonso <diego@diegoalonso.net>
2012-06-26 22:18:29 +1000

    Adding identation as block indicator

--- spec/toy_lang/scanner_spec.rb
+++ spec/toy_lang/scanner_spec.rb
@@ -12,7 +12,7 @@ describe ToyLang::Scanner do
   end
 
   it "clears white spaces" do
-    @scanner.set_program("  \r\f\t  ")
+    @scanner.set_program("\r\f\t  ")
     assert_token_is :eof
   end
 
@@ -73,15 +73,15 @@ describe ToyLang::Scanner do
 
   it "scans small program" do
     @scanner.set_program """
-      def method {
-        return 9
-      }
-      """
+def method
+  return 9
+
+"""
     assert_token_is :new_line
     assert_token_is :def
     assert_token_and_content_is :id, "method"
-    assert_token_is :open_block
     assert_token_is :new_line
+    assert_token_is :open_block
     assert_token_is :return
     assert_token_and_content_is :number, "9"
     assert_token_is :new_line
